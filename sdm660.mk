64ef1571e01b6a26636cc2837bddd3ecbe3460b4
--no-edit=Arian <arian.kulmer@web.de>
2022-05-28 18:35:30 +0530

    sdm660-common: Don't append -qti to power HAL name
    because: https://github.com/AospExtended/platform_vendor_qcom_opensource_power/blob/12.1.x/Android.mk#L133
    
    Change-Id: Icaa436d9961bb7ba0e560b3d03da73752f1e15a0
    
    Signed-off-by: clhex <clhexftw@protonmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -335,7 +335,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power-service \
     vendor.qti.hardware.perf@2.2.vendor
 
 # Public Libraries
