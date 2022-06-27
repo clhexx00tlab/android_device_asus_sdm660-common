6d9719a6acd8a3e33cb0c9fd15f07097c83a8e15
clhex <clhexftw@protonmail.com>
2022-06-27 15:37:29 +0530

    sdm660-common: Nuke LiveDisplay
    This reverts commit ae92305.

--- sdm660.mk
+++ sdm660.mk
@@ -226,10 +226,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.asus_sdm660
 
-# LiveDisplay native
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.omx@1.0-impl \
